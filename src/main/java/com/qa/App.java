ed93b5b63facd2eba5d03148c8c5b14104253efb
Helen Broderick <helenbroderick14@gmail.com>
2023-09-12 14:27:49 +0100

    updated to add in extra lines

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,19 +9,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
+        // Use ctrl/ to get the comment syntax for an inline comment and add in your comment
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
+        system.out.println(sayHelloToSomeone("Scooby Doo"))
+        system.out.println(sayGoodbye("Charlie"))
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayGoodbye());
+        System.out.println(sayGoodbye("World"));
     }
 
     public static String sayHello(){
         return "Hello World!";
     }
 
-    public static String sayGoodbye(){
-        return "Goodbye World!";
+    public static String sayGoodbye(Sting name){
+        return "Goodbye " + name;
     }
 
     public static String sayHelloToSomeone(String name){
